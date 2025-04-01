b7845093a09f48216ea2d9d2fb97977be32fa4b0
Lightart <2114419246@qq.com>
2025-04-01 22:10:33 +0800

    project1:MaxArrayDeque

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,28 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+
+    private Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> c) {
+        comparator = c;
+    }
+
+    public T max() {
+        return max(comparator);
+    }
+
+    public T max(Comparator<T> c) {
+        T res = null;
+        for(int i = 0; i < size(); i++) {
+            T t = get(i);
+            int cmp = c.compare(res, t);
+            if(cmp > 0) {
+                res = t;
+            }
+        }
+        return res;
+    }
+}
