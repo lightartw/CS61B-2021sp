234a6b40986047c36f25f528aee85dc9634c49c0
Lightart <2114419246@qq.com>
2025-04-01 19:24:01 +0800

    lab4:A,buggy lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,11 +5,11 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n%2 == 0) {
-            return n/2;
+        if (n == 128) {
+            return 1;
         }
         else{
-            return 3 * n + 1;
+            return n * 2;
         }
     }
 
